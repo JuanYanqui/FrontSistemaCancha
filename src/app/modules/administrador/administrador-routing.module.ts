6501590d8249f5562c3712d3c069120d92462b91
Juan Yanqui <juan.yanqui.est@tecazuay.edu.ec>
2023-02-10 23:09:36 -0500

    Cambios de crud completo de establecimiento
    Cambios de crud completo de establecimiento y ventanas

--- src/app/modules/administrador/administrador-routing.module.ts
+++ src/app/modules/administrador/administrador-routing.module.ts
@@ -6,10 +6,6 @@ const routes: Routes = [
     path: 'bienvenida',
     loadChildren: () => import("./bienvenida-administrador/bienvenida-administrador.module").then(m => m.BienvenidaAdministradorModule)
   },
-  {
-    path: 'casa',
-    loadChildren: () => import("./casa/casa.module").then(m => m.CasaModule)
-  },
 ];
 
 @NgModule({
