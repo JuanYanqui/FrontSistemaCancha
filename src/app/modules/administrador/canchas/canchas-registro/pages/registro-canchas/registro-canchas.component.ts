9674277ff8957db91ecd3c95736fabc977e7a640
WI1L0 <111997657+WI1L0@users.noreply.github.com>
2023-02-16 08:35:41 -0500

    mm
    jaja :)

--- src/app/modules/administrador/canchas/canchas-registro/pages/registro-canchas/registro-canchas.component.ts
+++ src/app/modules/administrador/canchas/canchas-registro/pages/registro-canchas/registro-canchas.component.ts
@@ -21,16 +21,21 @@ export class RegistroCanchasComponent {
 
 
   blockSpecial: RegExp = /^[^<>*!]+$/
-  date: Date = new Date("2018-03-16");
+
   verfNombres: any;
   verfDescripcion: any;
   verftarifa: any;
   verfancho: any;
   verfalto: any;
+<<<<<<< Updated upstream
+=======
+  verfoto: any;
+>>>>>>> Stashed changes
 
+  creareliminar: boolean = false;
 
   constructor(private cargarScripts: CargarScriptsService, private toastr: ToastrService, private fotoService: FotoService, private establecimientoService: EstablecimientoService, private canchasService: CanchasService, private router: Router) {
-    cargarScripts.Carga(["register-canchas.component"])
+    cargarScripts.Carga(["registro-canchas.component"])
   }
 
 
@@ -38,13 +43,17 @@ export class RegistroCanchasComponent {
     this.cancha.nombre = '';
     this.cancha.descripcion = '';
     this.cancha.tarifa = '';
+    this.cancha.foto = '';
     this.cancha.ancho = 0;
     this.cancha.altura = 0;
 
   }
 
   registrarCancha() {
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
     if (this.cancha.nombre == null || this.cancha.nombre.length == 0) {
       this.verfNombres = 'ng-invalid ng-dirty';
       this.toastr.error("Campo nombres vacio!", "Error!");
@@ -71,11 +80,17 @@ export class RegistroCanchasComponent {
       this.toastr.error("Campo alto de cancha debe ser mayor a cero!", "Error!");
     }
 
-    if (this.cancha.nombre === '' || this.cancha.descripcion === '' || this.cancha.tarifa === '' || this.cancha.ancho === 0 || this.cancha.altura === 0
-      || this.cancha.nombre === null || this.cancha.descripcion === null || this.cancha.tarifa === null || this.cancha.ancho === null || this.cancha.altura === null) {
+    if (this.cancha.foto === '' || this.cancha.foto === null) {
+      this.verfalto = 'ng-invalid ng-dirty';
+      this.toastr.error("Noto de la cancha no cargada!", "Error!");
+    }
+
+    if (this.cancha.nombre === '' || this.cancha.descripcion === '' || this.cancha.tarifa === '' || this.cancha.foto === '' || this.cancha.ancho === 0 || this.cancha.altura === 0
+      || this.cancha.nombre === null || this.cancha.descripcion === null || this.cancha.tarifa === null || this.cancha.foto === null || this.cancha.ancho === null || this.cancha.altura === null) {
 
       this.toastr.warning("Verifique que esten correctos los campos")
     } else {
+<<<<<<< Updated upstream
       this.canchasService.postCanchas(this.cancha).subscribe(
         data => {
           console.log(data);
@@ -110,6 +125,67 @@ export class RegistroCanchasComponent {
   selectedFile!: File;
 
   public imageSelected(event: any) {
+=======
+      this.establecimientoService.getEstablecimiento().subscribe(
+        data => {
+
+          for (let a = 0; a < data.length; a++) {
+            if (data[a].persona?.idPersona == Number(localStorage.getItem("localIdPersona"))) {
+              this.establecimiento.idEstablecimiento = data[a].idEstablecimiento;
+              this.establecimiento.ruc = data[a].ruc;
+              this.establecimiento.nombre = data[a].nombre;
+              this.establecimiento.puntuacion = data[a].puntuacion;
+              this.establecimiento.horaApertura = data[a].horaApertura;
+              this.establecimiento.horaCierre = data[a].horaCierre;
+              this.establecimiento.bar = data[a].bar;
+              this.establecimiento.estacionamiento = data[a].estacionamiento;
+              this.establecimiento.vestidores = data[a].vestidores;
+              this.establecimiento.banios = data[a].banios;
+              this.establecimiento.estado = data[a].estado;
+              this.establecimiento.provincia = data[a].provincia;
+              this.establecimiento.ciudad = data[a].ciudad;
+              this.establecimiento.direccion = data[a].direccion;
+              this.establecimiento.codigoPostal = data[a].codigoPostal;
+              this.establecimiento.fotoestablecimiento = data[a].fotoestablecimiento;
+              this.establecimiento.persona = data[a].persona;
+              this.establecimiento.ubicacion = data[a].ubicacion;
+            }
+          }
+
+          this.cancha.establecimiento = this.establecimiento;
+
+          this.canchasService.postCanchas(this.cancha).subscribe(
+            data => {
+              this.cancha.nombre = data.nombre;
+              this.cancha.descripcion = data.descripcion;
+              this.cancha.tarifa = data.tarifa;
+              this.cancha.altura = data.altura;
+              this.cancha.ancho = data.ancho;
+              this.cancha.vacante = true;
+              this.cancha.foto = this.nombre_orignal;
+
+              Swal.fire("La cancha se creo exitosamente")
+            }
+          )
+
+        }
+      )
+    }
+  }
+
+
+    // IMAGEN
+    image!: any;
+    file: any = '';
+
+    // CAPTURO EL ARCHIVO
+    nombre_orignal: string = "";
+
+    cap_nombre_archivo: any;
+    selectedFile!: File;
+  
+    public imageSelected(event: any) {
+>>>>>>> Stashed changes
 
     this.selectedFile = event.target.files[0];
 
@@ -135,5 +211,4 @@ export class RegistroCanchasComponent {
   cargarImagen() {
     this.fotoService.guararImagenes(this.selectedFile);
   }
-
 }
